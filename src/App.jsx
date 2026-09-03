6330e91e616b146d5b52a438eb73513fbaac6d57
umiphos <umiphos@gmail.com>
2026-09-03 00:02:14 -0600

    [ADD] login and register structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+
 function App() {
+  const [page, setPage] = useState('registro')
+
   return (
-    <h1>CATÁLOGO</h1>
+    <main>
+      {page === 'registro' && (
+        <section>
+          <h1>Registro</h1>
+          <button onClick={() => setPage('login')}>
+            Ir a login
+          </button>
+        </section>
+      )}
+
+      {page === 'login' && (
+        <section>
+          <h1>Login</h1>
+          <button onClick={() => setPage('registro')}>
+            Volver a registro
+          </button>
+        </section>
+      )}
+    </main>
   )
 }
 
